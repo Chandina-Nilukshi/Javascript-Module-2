ec13e6bdd62c6439ae7e7f8d6a09fb2b712056f2
Chandina-Nilukshi <chandinn@metropolia.fi>
2024-11-13 20:34:13 +0530

    Initial commit

---
+++ Task8/Task8.js
@@ -0,0 +1,17 @@
+'use strict'
+
+function concat(array){
+    let long = ""
+    for(let i = 0; i < array.length; i++){
+        long += array[i]
+    }
+    return long
+}
+
+function main(){
+    let names = ["Johnny", "DeeDee","Joey", "Marky"]
+    document.querySelector("#print").innerHTML = concat(names)
+}
+
+main()
+
